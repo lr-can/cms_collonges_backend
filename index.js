cf7ffa267f3b48135027f71c35c5448000c6d570
lr-can <103670765+lr-can@users.noreply.github.com>
2024-04-09 19:03:34 +0200

    added CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 const peremptions = require("./routes/peremptions");
 const peremptionsids = require("./routes/peremptionsids");
 
 app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: true,
