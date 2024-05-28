647b05617769ba80bbd15dd5ab27093e0a3aa3b9
lr-can <103670765+lr-can@users.noreply.github.com>
2024-05-28 19:43:43 +0200

    modified

--- routes/generatePDF.js
+++ routes/generatePDF.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path'); 
 const generatePDFRecap = require('../services/generateRecap');
 
 router.get('/', async (req, res) => {
     try {
-        await generatePDFRecap.generatePDFRecap();
-        const filePath = path.join(__dirname, '../recap.pdf'); 
-        res.sendFile(filePath); 
+        res.send(await generatePDFRecap.generatePDFRecap());
+         
     } catch (err) {
         console.error(err);
         res.status(500).send('Erreur du serveur');
