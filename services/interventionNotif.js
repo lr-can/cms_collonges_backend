8bf55f490f39c96697dbd4cc5e759d09946b3d49
lr-can <103670765+lr-can@users.noreply.github.com>
2024-06-14 22:30:30 +0200

    Does not work arghhh

--- services/interventionNotif.js
+++ services/interventionNotif.js
@@ -28,7 +28,7 @@ async function insertInterventionNotif(data) {
         });
         console.log('Row appended successfully!');
         
-        return response;
+        return data;
     } catch (err) {
         console.error('Error appending row:', err);
         throw err; // Renvoie l'erreur pour être gérée par l'appelant
