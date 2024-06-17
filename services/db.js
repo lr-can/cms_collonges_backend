f7870859db7dbc82d0267c2a7ee4e9e5c40833e0
lr-can <103670765+lr-can@users.noreply.github.com>
2024-06-17 18:37:20 +0200

    forcing to close connection even when there is an error

--- services/db.js
+++ services/db.js
@@ -3,10 +3,15 @@ const config = require('../config');
 
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
-
-  await connection.end();
-  return results;
+  try {
+    const [results, ] = await connection.execute(sql, params);
+    return results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await connection.end();
+  }
 }
 
 module.exports = {
