38df928a2c93cba7350fa268fb49cc5ad3a2b76a
lr-can <103670765+lr-can@users.noreply.github.com>
2024-10-22 14:52:46 +0200

    added functionality for fire hydrants

--- services/formation.js
+++ services/formation.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const turf = require('@turf/turf');
+let fetch;
 
 const telexFirstPartGeoJson = JSON.parse(fs.readFileSync('ressources/firstPart.geojson', 'utf8'));
 const telexSecondPartGeoJson = JSON.parse(fs.readFileSync('ressources/secondPart.geojson', 'utf8'));
@@ -43,12 +44,56 @@ function getClosestFeature(geoJson, point) {
     }
 }
 
+async function getClosestFireHydrants(lon, lat) {
+
+    const OVERPASS_API_URL = 'https://overpass-api.de/api/interpreter';
+    if (!fetch) {
+        fetch = (await import('node-fetch')).default;
+    }
+        const query = `
+            [out:json];
+            node
+              ["emergency"="fire_hydrant"]
+              (around:200,${lat},${lon});
+            out body;
+        `;
+        const response = await fetch(OVERPASS_API_URL, {
+            method: 'POST',
+            body: query,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch fire hydrants from OpenStreetMap');
+        }
+
+        const data = await response.json();
+        const point = turf.point([lon, lat]);
+        const hydrants = data.elements.map(element => ({
+            id: element.id,
+            lat: element.lat,
+            lon: element.lon,
+            tags: element.tags,
+            distance: turf.distance(point, turf.point([element.lon, element.lat]))
+        }));
+        hydrants.sort((a, b) => a.distance - b.distance);
+        return hydrants.map(hydrant => ({
+            id: hydrant.id,
+            numero: hydrant.tags.ref,
+            distance: Math.round(hydrant.distance * 1000)
+        }));
+    }
+
 async function getMapCoordinates(lon, lat) {
     const firstPart = await getFirstPartGeoJson(lon, lat);
     const firstPartString = firstPart && firstPart.est ? "est. " + (firstPart.data && firstPart.data.properties ? firstPart.data.properties.assigned_data : '') : firstPart && firstPart.properties ? firstPart.properties.assigned_data : '';
     const secondPart = await getSecondPartGeoJson(lon, lat);
     const secondPartString = secondPart && secondPart.properties ? secondPart.properties.assigned_data : '';
-    return { coordinates: { lon, lat }, mapCoordinates: `${firstPartString} ${secondPartString}`.replace("est.  ", "inconnu") };
+    return {
+        coordinates: { lon, lat },
+        mapCoordinates: `${firstPartString} ${secondPartString}`.replace("est.  ", "inconnu"),
+        fireHydrants: await getClosestFireHydrants(lon, lat)
+    };
 }
 
 module.exports = {
