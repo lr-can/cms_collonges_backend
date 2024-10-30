ee83851f1d76897a8ab54454dd86fde34b70fac2
lr-can <103670765+lr-can@users.noreply.github.com>
2024-10-30 13:40:06 +0100

    adjusting again

--- routes/vehiculesAffectation.js
+++ routes/vehiculesAffectation.js
@@ -5,8 +5,8 @@ const form = require('../services/formation');
 router13.post('/', async function(req, res, next) {
     try {
       const matricules = req.body.matricules;
-      const gfos = req.body.gfos;
-      res.json(await form.assignAgentsToVehicles(matricules, gfos));
+      const engins = req.body.gfos;
+      res.json(await form.assignAgentsToVehicles(matricules, engins));
     } catch (err) {
       console.error(`Error while db`, err.message);
       next(err);
