104b95fedc1dd2ed4e596c930be0cc4b0190f21b
lr-can <103670765+lr-can@users.noreply.github.com>
2024-10-30 16:02:56 +0100

    modifiying

--- routes/vehiculesAffectation.js
+++ routes/vehiculesAffectation.js
@@ -6,8 +6,8 @@ router13.post('/', async function(req, res, next) {
     try {
       const matricules = req.body.matricules;
       const codeSinistre = req.body.codeSinistre;
-      const personnalise = req.body.personnalise;
-      res.json(await form.assignAgentsToVehicles(matricules, codeSinistre, personnalise));
+      const personnalises = req.body.personnalise;
+      res.json(await form.assignAgentsToVehicles(matricules, codeSinistre, personnalises));
     } catch (err) {
       console.error(`Error while db`, err.message);
       next(err);
