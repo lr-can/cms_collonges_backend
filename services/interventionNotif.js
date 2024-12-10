9ad21dd451f0d5b3edb6d3c4e60b131b8b95a535
lr-can <103670765+lr-can@users.noreply.github.com>
2024-12-10 10:48:04 +0100

    readed giveInterventionType

--- services/interventionNotif.js
+++ services/interventionNotif.js
@@ -178,6 +178,49 @@ async function findInterventionCoordinates(adress){
         throw new Error('Unable to find coordinates for the given address');
     }
 }
+async function giveInterventionType(titre) {
+    let interTypeDictionnary = {};
+    if (!fetch) {
+        fetch = (await import('node-fetch')).default;
+    };
+    try {
+        const response = await fetch('https://opensheet.elk.sh/19kgbE-Z4kIbM49-rVE0hv9ihMBv_a5hOzP9DAa1CHt8/1');
+        const data = await response.json();
+
+        data.forEach(row => {
+            const interventionCode = row['codeSinistre']; // Correction ici
+            const libelleMajSinistre = row['libelleMajSinistre'];
+            let value = "";
+
+            if (interventionCode.startsWith('1')) {
+                value = "SSUAP";
+            } else if (interventionCode.startsWith('2')) {
+                if (/^2[AB]\d{2}$/.test(interventionCode)) {
+                    value = "Violences_Urbaines";
+                } else {
+                    value = "Accident";
+                }
+            } else if (interventionCode.startsWith('3')) {
+                if (interventionCode.length === 3) {
+                    value = "Violences_Urbaines_Graves";
+                } else {
+                    value = "Incendie";
+                }
+            } else if (interventionCode.startsWith('4') || interventionCode.startsWith('5')) {
+                value = "PPBE";
+            }
+
+            interTypeDictionnary[libelleMajSinistre] = value;
+        });
+
+        const type = interTypeDictionnary[titre];
+        return { type: type };
+
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        throw err;
+    }
+}
 
 async function insertSmartemisResponse(data) {
     const privateKey = config.google.private_key.replace(/\\n/g, '\n');
