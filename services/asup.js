39233fbbf6e4d7beb6ad011e4bbdb2fac7a280e8
lr-can <103670765+lr-can@users.noreply.github.com>
2025-04-01 09:06:27 +0200

    updated pdf generation

--- services/asup.js
+++ services/asup.js
@@ -3,7 +3,7 @@ const helper = require('../helper');
 const config = require('../config');
 const { google } = require('googleapis');
 const fs = require('fs');
-const pdf = require('html-pdf');
+const puppeteer = require('puppeteer');
 let fetch
 
 
@@ -1146,7 +1146,7 @@ async function generatePDF(){
                             <div class="status3-content-item"  style="text-align: left">${row.numLot}</div>
                             <div class="status3-content-item">${new Date(row.datePeremption).toLocaleDateString('fr-FR')}</div>
                             <div class="status3-content-item"  style="text-align: left">${row.createur.nomAgent} ${row.createur.prenomAgent}</div>
-                            <div class="status3-content-item">${row.matriculeRemplaceur}</div>
+                            <div class="status3-content-item">${row.matriculeRemplaceur !== null? row.matriculeRemplaceur : "à remplacer" }</div>
                         </div>`;
                 });
                 htmlBody += `</div>`;
@@ -1182,26 +1182,44 @@ async function generatePDF(){
                 htmlBody += `<p class="status5-message">Aucun médicament trouvé.</p>`;
             }
             htmlBody += `</div></body></html>`;
+            const finalHTML = htmlHeader + htmlBody;
 
                     
-        // Combinaison de l'en-tête et du corps pour générer le PDF
-        const finalHTML = htmlHeader + htmlBody;
-        return new Promise((resolve, reject) => {
-            const options = {
-                format: 'A4',
-                orientation: 'landscape',
-                border: '1.5cm'
-            };
-
-            pdf.create(finalHTML, options).toBuffer((err, buffer) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(buffer);
-                }
-            });
+     
+    try {
+        // Lancez Puppeteer
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // Ajoutez cette ligne si nécessaire
+                '--disable-gpu'
+            ],
+            headless: true,
         });
+        const page = await browser.newPage();
+
+        // Chargez le contenu HTML dans la page
+        await page.setContent(finalHTML, { waitUntil: 'load' });
+
+        // Génération du PDF
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            landscape: true,
+            printBackground: true,
+        });
+
+        await browser.close();
+        return pdfBuffer;
+    } catch (error) {
+        console.error('Erreur lors de la génération du PDF :', error);
+        throw error;
     }
+}
     
 
 
